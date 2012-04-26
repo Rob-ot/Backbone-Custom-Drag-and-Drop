2043b7e352292d3f22487341fecc445874b88c40
Rob Middleton <rob@i.tv>
2012-04-26 13:54:37 -0600

    first

---
+++ Draggable.js
@@ -0,0 +1,112 @@
+define([
+  'wrap!underscore',
+  'jquery',
+  './Droppable'
+],
+function (
+  _,
+  $,
+  Droppable
+  ) {
+  $(window).bind('touchstart', function () {}) // ios hack
+  return {
+    setDraggable: function (draggable) {
+      if (draggable) {
+        this.enableDraggable()
+      }
+      else {
+        this.disableDraggable()
+      }
+    },
+
+    enableDraggable: function () {
+      if (this.draggable) return
+      this.draggable = true
+
+      if (!this.x) this.x = 0
+      if (!this.y) this.y = 0
+
+      if (!this._draggable_downBound) {
+        this._draggable_downBound = _.bind(this._draggable_down, this)
+        // just do them all now
+        this._draggable_moveBound = _.bind(this._draggable_mouseMove, this)
+        this._draggable_upBound = _.bind(this._draggable_mouseUp, this)
+      }
+
+      this.$el.bind("mousedown touchstart", this._draggable_downBound)      
+    },
+
+    disableDraggable: function () {
+      if (!this.draggable) return
+      this.draggable = false
+      this.$el.unbind("mousedown", this._draggable_downBound)
+    },
+
+    _draggable_down: function (e) {
+      e = e.originalEvent
+      if (e.type === 'mousedown' && e.which !== 1) return
+      e.preventDefault()
+      this.trigger("dragstart")
+
+      if (e.targetTouches) {
+        this._draggable_startDragX = e.targetTouches[0].pageX
+        this._draggable_startDragY = e.targetTouches[0].pageY
+
+        $(window).bind('touchmove', this._draggable_moveBound)
+          .bind('touchend touchcancel', this._draggable_upBound)
+      }
+      else {
+        this._draggable_startDragX = e.clientX
+        this._draggable_startDragY = e.clientY
+        
+        $(window).mousemove(this._draggable_moveBound)
+          .mouseup(this._draggable_upBound)
+          .mouseleave(this._draggable_upBound)
+      }
+
+      this._draggable_startTime = new Date().getTime()
+    },
+
+    _draggable_mouseMove: function (e) {
+      e = e.originalEvent
+      var x, y, changeX, changeY
+
+      if (e.targetTouches) {
+        x = e.targetTouches[0].pageX
+        y = e.targetTouches[0].pageY
+      }
+      else {
+        x = e.clientX
+        y = e.clientY
+      }
+
+      changeX = x - this._draggable_startDragX
+      changeY = y - this._draggable_startDragY
+
+      this.x += changeX
+      this.y += changeY
+
+      this.trigger("dragmove", changeX, changeY)
+
+      Droppable._droppableOnDragMove(this, this.x, this.y, this.width, this.height)
+
+      this._draggable_startDragX = x
+      this._draggable_startDragY = y
+    },
+
+    _draggable_mouseUp: function () {
+      this.trigger("dragend")
+
+      Droppable._droppableOnDragEnd(this, this.x, this.y, this.width, this.height)
+
+      var dragtime = new Date().getTime() - this._draggable_startTime
+
+      if (dragtime < 130) {
+        this.trigger("dragclick")
+      }
+
+      $(window).unbind("mousemove touchmove", this._draggable_moveBound)
+        .unbind("mousemove mouseup mouseleave touchend touchcancel", this._draggable_upBound)
+    }
+  }
+})
---
+++ Droppable.js
@@ -0,0 +1,94 @@
+define([
+  'wrap!underscore'
+],
+function (
+  _
+) {
+  var droppables = [], currentHover = null
+
+  function pointInside (pointX, pointY, x, y, w, h) {
+    return x <= pointX && x + w >= pointX &&
+      y <= pointY && y + h >= pointY
+  }
+
+  function isHovering (droppable, dragX, dragY, dragW, dragH) {
+    return pointInside(dragX, dragY, droppable.x, droppable.y, droppable.width, droppable.height) // top left
+      || pointInside(dragX + dragW, dragY, droppable.x, droppable.y, droppable.width, droppable.height) // top right
+      || pointInside(dragX, dragY + dragH, droppable.x, droppable.y, droppable.width, droppable.height) // bottom left
+      || pointInside(dragX + dragW, dragY + dragH, droppable.x, droppable.y, droppable.width, droppable.height) // bottom right
+  }
+
+  function distance (droppable, dragX, dragY, dragW, dragH) {
+    if (!isHovering(droppable, dragX, dragY, dragW, dragH)) return -1
+    return Math.sqrt(Math.pow(dragX - droppable.x, 2) + Math.pow(dragY - droppable.y, 2))
+  }
+
+  function closestDroppable (x, y, w, h) {
+    var dist, closestDistance, droppable, closestView = null
+    for (var i = 0; i < droppables.length; i++) {
+      droppable = droppables[i]
+      dist = distance(droppable, x, y, w, h)
+      if (dist !== -1) {
+        if (!closestView || dist < closestDistance) {
+          closestDistance = dist
+          closestView = droppable
+        }
+      }
+    }
+
+    return closestView
+  }
+
+  return {
+    setDroppable: function (droppable, x, y, w, h) {
+      if (droppable) {
+        this.enableDroppable(x, y, w, h)
+      }
+      else {
+        this.disableDroppable()
+      }
+    },
+
+    enableDroppable: function (x, y, w, h) {
+      if (this.droppable) return
+      this.droppable = true
+
+      if (!this.x) this.x = 0
+      if (!this.y) this.y = 0
+
+      droppables.push(this)
+    },
+
+    disableDroppable: function () {
+      if (!this.droppable) return
+
+      var index = _.indexOf(droppables, this)
+      if (index === -1) return console.error("couldnt find droppable, wtf")
+
+      droppables.splice(index, 1)
+
+      this.droppable = false
+    },
+
+    _droppableOnDragEnd: function (view, x, y, w, h) {
+      var closestView = closestDroppable(x, y, w, h)
+
+      if (currentHover) currentHover.trigger('dropunhover')
+      currentHover = null
+
+      if (closestView) {
+        closestView.trigger('drop', view)
+      }
+    },
+
+    _droppableOnDragMove: function (view, x, y, w, h) {
+      var closestView = closestDroppable(x, y, w, h)
+
+      if (currentHover === closestView) return
+
+      if (currentHover) currentHover.trigger('dropunhover')
+      if (closestView) closestView.trigger('drophover', view)
+      currentHover = closestView
+    }
+  }
+})
